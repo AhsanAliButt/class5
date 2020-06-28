c229f3b2251a09360af205ac15b4b6003051eb70
ahsan <ahsanbutt515@icloud.com>
2020-06-28 13:28:05 +0500

    update all

--- class5/src/App.js
+++ class5/src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import Parent from'./Parent';
 import CounterContext from './CounterContext';
 
 function App() {
+  //let [count, Setcount]=useState(30)
+  let countState=useState(25)
   return (
-    <CounterContext.Provider value={10}>
+    <CounterContext.Provider value={countState}>
       <div>
       <h1>Hellow World</h1>
       <Parent name="Ahsan"/>
+        <h3>PIAIC TEACHERS ARE BEST</h3>
       </div>
     </CounterContext.Provider>
   
--- class5/src/Child.js
+++ class5/src/Child.js
@@ -1,13 +1,15 @@
 import React, { useContext } from 'react';
-import countercontext from './CounterContext';
+import counterContext from './CounterContext';
 
 const Child = (props) =>{
-let counterValue = useContext(countercontext)
+let counterValue = useContext(counterContext)
 console.log(counterValue)
 return(
 <div>
 <h1>Hi i am First Child</h1>
 <h2>Hi my name is {props.name}</h2>
+<h3>Counter Value is :{counterValue[0]}</h3>
+<button onClick={()=> {counterValue[1](++counterValue[0])}}>increament</button>
 </div>
 )
 }
